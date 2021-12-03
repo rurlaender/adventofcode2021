875f3ce32ca221f21a973090da86ec20b909b810
Jochen Rurlaender <jochen@rurlaender.org>
2021-12-03 16:37:12 +0100

    Change a view things

--- day_03/script_02.py
+++ day_03/script_02.py
@@ -1,17 +1,19 @@
 import math
 
-nbrs = []
-with open('data.txt', 'r') as data:
-    for line in data:
-        nbrs.append(int(line, 2))
 
+numbers = []
+with open("day_03/data.txt", "r") as data:
+    for line in data:
+        numbers.append(int(line, 2))
+
+nbrs = numbers[:]
 for pos in range(11, -1, -1):
     counter = 0
     for nbr in nbrs:
         if nbr & int(math.pow(2, pos)) == int(math.pow(2, pos)):
             counter += 1
 
-    if counter >= len(nbrs)-counter:
+    if counter >= len(nbrs) - counter:
         bit_val = int(math.pow(2, pos))
     else:
         bit_val = 0
@@ -25,11 +27,9 @@ for pos in range(11, -1, -1):
         break
 
 
-a = nbrs[0]
+oxygen = nbrs[0]
 
-with open('data.txt', 'r') as data:
-    for line in data:
-        nbrs.append(int(line, 2))
+nbrs = numbers[:]
 
 for pos in range(11, -1, -1):
     counter = 0
@@ -37,7 +37,7 @@ for pos in range(11, -1, -1):
         if nbr & int(math.pow(2, pos)) == 0:
             counter += 1
 
-    if counter <= len(nbrs)-counter:
+    if counter <= len(nbrs) - counter:
         bit_val = 0
     else:
         bit_val = int(math.pow(2, pos))
@@ -49,6 +49,6 @@ for pos in range(11, -1, -1):
     nbrs = new_nbrs[:]
     if len(nbrs) == 1:
         break
-b = nbrs[0]
+co2 = nbrs[0]
 
-print(a, b, a*b)
+print(oxygen, co2, oxygen * co2)
